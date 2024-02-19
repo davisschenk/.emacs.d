099711f5ea7d092602b02190e15737b13d52ae5a
Davis Schenkenberger <davisschenk@gmail.com>
2024-02-19 13:38:00 -0700

    Add elisp mode configs

--- lisp/davis-lisp.el
+++ lisp/davis-lisp.el
@@ -6,4 +6,18 @@
   :commands (elisp-autofmt-mode elisp-autofmt-buffer)
   :hook (emacs-lisp-mode . elisp-autofmt-mode))
 
+(use-package elisp-mode
+  :ensure nil
+  :general
+  (global-leader
+    :major-modes '(emacs-lisp-mode lisp-interaction-mode t)
+    :keymaps     '(emacs-lisp-mode-map lisp-interaction-mode-map)
+    "e"  '(:ignore t :which-key "eval")
+    "eb" 'eval-buffer
+    "ed" 'eval-defun
+    "ee" 'eval-expression
+    "ep" 'pp-eval-last-sexp
+    "es" 'eval-last-sexp
+    "i"  'elisp-index-search))
+
 (provide 'davis-lisp)
